405f1090cc7eeed8133e4033e575183023c2c329
Junior Martins <amjr.box@gmail.com>
2023-03-01 08:20:39 -0300

    chore: roda a primeira migration do banco

---
+++ prisma/migrations/20230312210600_init/migration.sql
@@ -0,0 +1,83 @@
+-- CreateTable
+CREATE TABLE `expenses` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `expense_type_id` INTEGER UNSIGNED NOT NULL,
+    `payment_methods_id` INTEGER UNSIGNED NOT NULL,
+    `expense_local_id` INTEGER UNSIGNED NULL,
+    `description` VARCHAR(255) NOT NULL,
+    `date` DATETIME(3) NOT NULL,
+    `amount` BIGINT NOT NULL,
+    `installments` INTEGER NOT NULL,
+    `installments_first_due_date` DATETIME(3) NOT NULL,
+    `observations` TEXT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `status` BOOLEAN NOT NULL DEFAULT true,
+
+    INDEX `expenses_expense_type_id_idx`(`expense_type_id`),
+    INDEX `expenses_payment_methods_id_idx`(`payment_methods_id`),
+    INDEX `expenses_expense_local_id_idx`(`expense_local_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `expense_types` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `description` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `status` BOOLEAN NOT NULL DEFAULT true,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `expense_locals` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `description` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `status` BOOLEAN NOT NULL DEFAULT true,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `installments` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `expense_id` INTEGER UNSIGNED NOT NULL,
+    `number` INTEGER NOT NULL,
+    `due_date` DATETIME(3) NOT NULL,
+    `amount` BIGINT NOT NULL,
+    `paid` BOOLEAN NOT NULL DEFAULT false,
+    `paid_date` DATETIME(3) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `status` BOOLEAN NOT NULL DEFAULT true,
+
+    INDEX `installments_expense_id_idx`(`expense_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `payment_methods` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `description` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `status` BOOLEAN NOT NULL DEFAULT true,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `expenses` ADD CONSTRAINT `expenses_expense_type_id_fkey` FOREIGN KEY (`expense_type_id`) REFERENCES `expense_types`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `expenses` ADD CONSTRAINT `expenses_payment_methods_id_fkey` FOREIGN KEY (`payment_methods_id`) REFERENCES `payment_methods`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `expenses` ADD CONSTRAINT `expenses_expense_local_id_fkey` FOREIGN KEY (`expense_local_id`) REFERENCES `expense_locals`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `installments` ADD CONSTRAINT `installments_expense_id_fkey` FOREIGN KEY (`expense_id`) REFERENCES `expenses`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
