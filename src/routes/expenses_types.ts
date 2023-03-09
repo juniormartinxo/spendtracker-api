4a9959baab7b5e682b4c01303659bc09d12570fc
Junior Martins <amjr.box@gmail.com>
2023-03-09 11:20:59 -0300

    refactor: altera os nomes das variáveis

--- src/routes/expenses_types.ts
+++ src/routes/expenses_types.ts
@@ -1,42 +1,42 @@
 import prisma from '../prisma/prisma'
 import { Router } from 'express'
 
-const expenseType = Router()
+const expenseTypeRoute = Router()
 
-expenseType.get(`/`, async (req, res) => {
-  const expenses = await prisma.expenseType.findMany({
+expenseTypeRoute.get(`/`, async (req, res) => {
+  const expenseType = await prisma.expenseType.findMany({
     where: { status: true },
   })
 
-  res.json(expenses)
+  res.json(expenseType)
 })
 
-expenseType.post(`/`, async (req, res) => {
-  const expense = await prisma.expenseType.createMany({ data: req.body, skipDuplicates: true })
+expenseTypeRoute.post(`/`, async (req, res) => {
+  const expenseType = await prisma.expenseType.createMany({ data: req.body, skipDuplicates: true })
 
-  res.json(expense)
+  res.json(expenseType)
 })
 
-expenseType.put(`/:id`, async (req, res) => {
+expenseTypeRoute.put(`/:id`, async (req, res) => {
   const { id } = req.params
 
-  const expense = await prisma.expenseType.update({
+  const expenseType = await prisma.expenseType.update({
     where: { id: Number(id) },
     data: { status: true },
   })
 
-  res.json(expense)
+  res.json(expenseType)
 })
 
-expenseType.delete(`/:id`, async (req, res) => {
+expenseTypeRoute.delete(`/:id`, async (req, res) => {
   const { id } = req.params
 
-  const expense = await prisma.expenseType.update({
+  const expenseType = await prisma.expenseType.update({
     where: { id: Number(id) },
     data: { status: false },
   })
 
-  res.json(expense)
+  res.json(expenseType)
 })
 
-export default expenseType
+export default expenseTypeRoute
