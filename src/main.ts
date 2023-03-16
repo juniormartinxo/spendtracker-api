4514344dab4e3e7bc9b22b8248e7622262a3517e
Junior Martins <amjr.box@gmail.com>
2023-03-16 08:11:24 -0300

    feat: implementa os erros comuns da aplicação

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
+import { ConflictInterceptor } from './common/errors/interceptors/conflict.interceptor'
+import { DatabaseInterceptor } from './common/errors/interceptors/database.interceptor'
+import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthorized.interceptor'
+import { NotFoundInterceptor } from './common/errors/interceptors/notfound.interceptor'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  )
+
+  app.useGlobalInterceptors(new ConflictInterceptor())
+  app.useGlobalInterceptors(new DatabaseInterceptor())
+  app.useGlobalInterceptors(new UnauthorizedInterceptor())
+  app.useGlobalInterceptors(new NotFoundInterceptor())
+
+  app.enableCors()
+
+  await app.listen(process.env.APP_PORT || 3000)
 }
-bootstrap();
+bootstrap()
