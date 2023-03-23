8f86d2f73d6053950f8f00e6d088a9d2b69755a8
Junior Martins <amjr.box@gmail.com>
2023-03-23 16:44:54 -0300

    feat: cria o module de payment_methods

---
+++ src/entities/payment_methods/payment_methods.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common'
+import { PaymentMethodsService } from './payment_methods.service'
+import { PaymentMethodsController } from './payment_methods.controller'
+import { PaymentMethodsRepository } from './repositories/payment_methods.repository'
+import { PrismaService } from '../../prisma/prisma.service'
+
+@Module({
+  controllers: [PaymentMethodsController],
+  providers: [PaymentMethodsService, PaymentMethodsRepository, PrismaService],
+})
+export class PaymentMethodsModule {}
