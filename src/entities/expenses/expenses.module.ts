ea8d1ec39f0d28ae5a736d8a0b330af2ce17c904
Junior Martins <amjr.box@gmail.com>
2023-03-30 16:14:23 -0300

    feat: cria o module de expenses

--- src/entities/expenses/expenses.module.ts
+++ src/entities/expenses/expenses.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common'
 import { ExpensesService } from './expenses.service'
 import { ExpensesController } from './expenses.controller'
+import { ExpensesRepository } from './repositories/expenses.repository'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 @Module({
   controllers: [ExpensesController],
-  providers: [ExpensesService],
+  providers: [ExpensesService, ExpensesRepository, PrismaService],
+  exports: [ExpensesService, ExpensesRepository],
 })
 export class ExpensesModule {}
