863df394c9a60e70fdc666d5be1b206006f9567d
Junior Martins <amjr.box@gmail.com>
2023-04-01 05:34:55 -0300

    feat: implementa o create DTO de exepensive_local

--- src/entities/expense_locals/dto/create-expense_local.dto.ts
+++ src/entities/expense_locals/dto/create-expense_local.dto.ts
@@ -1,1 +1,12 @@
-export class CreateExpenseLocalDto {}
+import { IsBoolean, IsDate, IsString } from 'class-validator'
+
+export class CreateExpenseLocalDto {
+  @IsString()
+  description: string
+  @IsDate()
+  created_at?: Date
+  @IsDate()
+  updated_at?: Date
+  @IsBoolean()
+  active?: boolean
+}
