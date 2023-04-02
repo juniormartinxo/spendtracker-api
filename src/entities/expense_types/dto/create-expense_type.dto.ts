1785a3587fec2b6641dd3684b07f92cf0a80dfb5
Junior Martins <amjr.box@gmail.com>
2023-04-02 05:39:22 -0300

    feat: implementa o create DTO de expense_types

--- src/entities/expense_types/dto/create-expense_type.dto.ts
+++ src/entities/expense_types/dto/create-expense_type.dto.ts
@@ -1,1 +1,14 @@
-export class CreateExpenseTypeDto {}
+import { IsBoolean, IsDate, IsString } from 'class-validator'
+export class CreateExpenseTypeDto {
+  @IsString()
+  description: string
+
+  @IsDate()
+  created_at?: Date
+
+  @IsDate()
+  updated_at?: Date
+
+  @IsBoolean()
+  active?: boolean
+}
