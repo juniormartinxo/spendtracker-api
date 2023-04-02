372ad91ba54bcde749e0d2b9363ef99a860abc07
Junior Martins <amjr.box@gmail.com>
2023-04-02 05:37:56 -0300

    feat: implementa o módulo de expense_types

--- src/entities/expense_types/expense_types.module.ts
+++ src/entities/expense_types/expense_types.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common'
 import { ExpenseTypesService } from './expense_types.service'
 import { ExpenseTypesController } from './expense_types.controller'
+import { ExpenseTypesRepository } from './repositories/expense_types.repository'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 @Module({
   controllers: [ExpenseTypesController],
-  providers: [ExpenseTypesService],
+  providers: [ExpenseTypesService, ExpenseTypesRepository, PrismaService],
+  exports: [ExpenseTypesService, ExpenseTypesRepository],
 })
 export class ExpenseTypesModule {}
