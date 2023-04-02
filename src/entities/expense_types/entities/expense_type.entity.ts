62430ce73a4d970fee28f2214cd3e101fd7408cb
Junior Martins <amjr.box@gmail.com>
2023-04-02 05:39:42 -0300

    feat: implementa a entity de expense_types

--- src/entities/expense_types/entities/expense_type.entity.ts
+++ src/entities/expense_types/entities/expense_type.entity.ts
@@ -1,1 +1,9 @@
-export class ExpenseType {}
+import { ExpenseType } from '.prisma/client'
+
+export class ExpenseTypeEntity implements ExpenseType {
+  id: number
+  description: string
+  created_at: Date
+  updated_at: Date
+  active: boolean
+}
