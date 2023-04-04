1eb530914342b1800061e88fccd8384151279c48
Junior Martins <amjr.box@gmail.com>
2023-04-04 07:51:17 -0300

    feat: implementa o módulo de expensive_local

--- src/entities/expense_locals/expense_locals.module.ts
+++ src/entities/expense_locals/expense_locals.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
 import { ExpenseLocalsService } from './expense_locals.service'
 import { ExpenseLocalsController } from './expense_locals.controller'
+import { ExpenseLocalsRepository } from './repositories/expense_locals.repository'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 @Module({
   controllers: [ExpenseLocalsController],
-  providers: [ExpenseLocalsService],
+  providers: [ExpenseLocalsService, ExpenseLocalsRepository, PrismaService],
 })
 export class ExpenseLocalsModule {}
