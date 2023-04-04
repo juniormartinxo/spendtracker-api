229c7098f7223adf631aaf1792d5ad371cfe2b40
Junior Martins <amjr.box@gmail.com>
2023-04-04 07:56:05 -0300

    refactor: melhora a resposta da API na DTO de payment_methods

--- src/entities/payment_methods/dto/create-payment_method.dto.ts
+++ src/entities/payment_methods/dto/create-payment_method.dto.ts
@@ -1,8 +1,11 @@
-import { IsString, IsInt, IsBoolean } from 'class-validator'
+import { IsString, IsInt, IsBoolean, IsNotEmpty } from 'class-validator'
 
 export class CreatePaymentMethodDto {
-  @IsString()
+  @IsString({ message: 'Nome deve ser uma string' })
+  @IsNotEmpty({ message: 'Nome não pode ser vazio' })
   description: string
-  @IsBoolean()
+
+  @IsBoolean({ message: 'Ativo deve ser um booleano' })
+  @IsNotEmpty({ message: 'Ativo não pode ser vazio' })
   active: boolean
 }
