5180f13650237516dc2a2aa07f0eb23601afb878
Junior Martins <amjr.box@gmail.com>
2023-04-04 07:48:30 -0300

    feat: melhora a resposta da API com o decorator ApiResponse da lib Swagger

--- src/entities/expense_locals/expense_locals.controller.ts
+++ src/entities/expense_locals/expense_locals.controller.ts
@@ -2,33 +2,40 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ExpenseLocalsService } from './expense_locals.service'
 import { CreateExpenseLocalDto } from './dto/create-expense_local.dto'
 import { UpdateExpenseLocalDto } from './dto/update-expense_local.dto'
+import { ApiResponse } from '@nestjs/swagger'
 
 @Controller('expense-locals')
 export class ExpenseLocalsController {
-  constructor(private readonly expenseLocalsService: ExpenseLocalsService) {}
+  constructor(private readonly service: ExpenseLocalsService) {}
 
+  @ApiResponse({ status: 409, description: 'Esta local de despesa já existe!' })
+  @ApiResponse({ status: 400, description: 'Bad request' })
   @Post()
-  create(@Body() createExpenseLocalDto: CreateExpenseLocalDto) {
-    return this.expenseLocalsService.create(createExpenseLocalDto)
+  create(@Body() dto: CreateExpenseLocalDto) {
+    return this.service.create(dto)
   }
 
+  @ApiResponse({ status: 409, description: 'Esta local de despesa já existe!' })
+  @ApiResponse({ status: 400, description: 'Bad request' })
   @Get()
-  findAll() {
-    return this.expenseLocalsService.findAll()
+  findAll(skip: number, take: number, order: string, direction: string) {
+    return this.service.findAll(skip, take, order, direction)
   }
 
+  @ApiResponse({ status: 409, description: 'Esta local de despesa já existe!' })
+  @ApiResponse({ status: 400, description: 'Bad request' })
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.expenseLocalsService.findOne(+id)
+    return this.service.findOne(+id)
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateExpenseLocalDto: UpdateExpenseLocalDto) {
-    return this.expenseLocalsService.update(+id, updateExpenseLocalDto)
+  update(@Param('id') id: string, @Body() dto: UpdateExpenseLocalDto) {
+    return this.service.update(+id, dto)
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.expenseLocalsService.remove(+id)
+    return this.service.remove(+id)
   }
 }
