e6e81dd1dec343442651ef3e7a737b994ed042f0
Junior Martins <amjr.box@gmail.com>
2023-04-04 07:52:51 -0300

    refactor: melhora as mensagens de retorno da API na DTO expense_type

--- src/entities/expense_types/dto/create-expense_type.dto.ts
+++ src/entities/expense_types/dto/create-expense_type.dto.ts
@@ -1,14 +1,15 @@
-import { IsBoolean, IsDate, IsString } from 'class-validator'
+import { IsBoolean, IsDate, IsNotEmpty, IsString } from 'class-validator'
 export class CreateExpenseTypeDto {
-  @IsString()
+  @IsString({ message: 'Descrição deve ser uma string' })
+  @IsNotEmpty({ message: 'Descrição não pode ser vazia' })
   description: string
 
-  @IsDate()
+  @IsDate({ message: 'Data de criação deve ser uma data' })
   created_at?: Date
 
-  @IsDate()
+  @IsDate({ message: 'Data de atualização deve ser uma data' })
   updated_at?: Date
 
-  @IsBoolean()
+  @IsBoolean({ message: 'Ativo deve ser um booleano' })
   active?: boolean
 }
