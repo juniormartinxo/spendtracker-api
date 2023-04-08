4ac2da776de989ca46eb7043522d62d07ffeaf5e
Junior Martins <amjr.box@gmail.com>
2023-04-08 18:34:39 -0300

    feat(auth): torna público o endpoint de login e cria o método me()
    Login fica público por sua característica e criado o método me() para retornar as informações do
    usuário para ele mesmo

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,25 @@
-import { Controller, HttpCode, HttpStatus, Post, Request, UseGuards } from '@nestjs/common'
+import { Controller, Get, HttpCode, HttpStatus, Post, Request, UseGuards } from '@nestjs/common'
+import { UserEntity } from 'src/entities/users/entities/user.entity'
 import { AuthService } from './auth.service'
+import { CurrentUser } from './decorator/current-user.decorator'
+import { IsPublic } from './decorator/is-public.decorator'
 import { LocalAuthGuard } from './guards/local-auth.guard'
-import { AutRequest } from './models/AuthRequest'
+import { AuthRequest } from './models/AuthRequest'
 
-@Controller()
+@Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @IsPublic()
+  @UseGuards(LocalAuthGuard)
   @Post('login')
   @HttpCode(HttpStatus.OK)
-  @UseGuards(LocalAuthGuard)
-  login(@Request() req: AutRequest) {
+  login(@Request() req: AuthRequest) {
     return this.authService.login(req.user)
+  }
+
+  @Get('me')
+  getMe(@CurrentUser() user: UserEntity): UserEntity {
+    return user //this.appService.getMe()
   }
 }
