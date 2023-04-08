bbe954a71cbc43efc2222dc5161fbc9b194de571
Junior Martins <amjr.box@gmail.com>
2023-04-07 22:13:31 -0300

    feat(auth): cria o controller de auth
    Criado o controller para controlar as ações de autenticação

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { LocalAuthGuard } from './guards/local-auth.guard'
+
+@Controller()
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(LocalAuthGuard)
+  async login() {
+    return await this.authService.login()
+  }
+}
