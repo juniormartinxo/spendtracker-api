977c646dc0d99346311ba7beeec0795ad01d94c9
Junior Martins <amjr.box@gmail.com>
2023-04-07 22:12:41 -0300

    feat(auth): adiciona AuthModule ao módulo principal da aplicação

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { ConfigModule } from '@nestjs/config'
-import { ExpensesModule } from './entities/expenses/expenses.module'
-import { ExpenseTypesModule } from './entities/expense_types/expense_types.module'
 import { ExpenseLocalsModule } from './entities/expense_locals/expense_locals.module'
+import { ExpenseTypesModule } from './entities/expense_types/expense_types.module'
+import { ExpensesModule } from './entities/expenses/expenses.module'
 import { InstallmentsModule } from './entities/installments/installments.module'
 import { PaymentMethodsModule } from './entities/payment_methods/payment_methods.module'
 import { UsersModule } from './entities/users/users.module'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { UsersModule } from './entities/users/users.module'
     InstallmentsModule,
     PaymentMethodsModule,
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
