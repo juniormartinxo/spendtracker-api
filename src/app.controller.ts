1a453d60fb10880aaadd113719b0c8d725d29ac4
Junior Martins <amjr.box@gmail.com>
2023-04-08 18:32:48 -0300

    refactor(app): torna pública o endpoint init

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common'
 import { AppService } from './app.service'
+import { IsPublic } from './auth/decorator/is-public.decorator'
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @IsPublic()
   @Get()
   init(): string {
     return this.appService.init()
