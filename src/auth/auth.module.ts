516511e22cfb0812b37df80a3c8c530392f18ebf
Junior Martins <amjr.box@gmail.com>
2023-04-08 18:36:42 -0300

    feat(auth): adiciona LoginValidationMiddleware, JWT_EXPIRES e JwtStrategy ao módulo auth
    - O LoginValidationMiddleware para validação de dados de login;
    - JWT_EXPIRES para regular o tempo de duração de um token; e
    - JwtStrategy que implementa uma estratégia de autenticação com JSON Web Token (JWT)
      para uso com o pacote passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,27 @@
-import { Module } from '@nestjs/common'
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
 import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
 import { UsersModule } from 'src/entities/users/users.module'
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
+import { LoginValidationMiddleware } from './middlewares/login-validation.middleware'
+import { JwtStrategy } from './strategies/jwt.strategy'
 import { LocalStrategy } from './strategies/local.strategy'
 
 @Module({
   imports: [
     UsersModule,
+    PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1d' },
+      signOptions: { expiresIn: process.env.JWT_EXPIRES },
     }),
   ],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   controllers: [AuthController],
 })
-export class AuthModule {}
+export class AuthModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoginValidationMiddleware).forRoutes('login')
+  }
+}
