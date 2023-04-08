017e1ddf9acf246a4c7c7b2c4470f52f7bcaf6dd
Junior Martins <amjr.box@gmail.com>
2023-04-07 22:57:41 -0300

    refactor(user): melhora a implementallção do DTO de user

--- src/entities/users/users.service.ts
+++ src/entities/users/users.service.ts
@@ -7,23 +7,27 @@ import { UsersRepository } from './repositories/users.repository'
 export class UsersService {
   constructor(private readonly repository: UsersRepository) {}
 
-  create(dto: CreateUserDto) {
-    return this.repository.create(dto)
+  async create(dto: CreateUserDto) {
+    return await this.repository.create(dto)
   }
 
   async findAll(skip: number, take: number, order: string, direction: string) {
     return await this.repository.findAll(skip, take, order, direction)
   }
 
-  findOne(uuid: string) {
-    return this.repository.findOne(uuid)
+  async findOne(uuid: string) {
+    return await this.repository.findOne(uuid)
   }
 
-  update(uuid: string, dto: UpdateUserDto) {
-    return this.repository.update(uuid, dto)
+  async findByEmail(email: string) {
+    return await this.repository.findByEmail(email)
   }
 
-  remove(uuid: string) {
-    return this.repository.remove(uuid)
+  async update(uuid: string, dto: UpdateUserDto) {
+    return await this.repository.update(uuid, dto)
+  }
+
+  async remove(uuid: string) {
+    return await this.repository.remove(uuid)
   }
 }
